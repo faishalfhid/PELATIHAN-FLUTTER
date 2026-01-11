3dbe3e50e196606be2893fbf59d8e5d8b87ae915
faishalfhid <faishalfh1.work@gmail.com>
2026-01-11 17:41:44 +0700

    Add Hello Wordl App

---
+++ 1. Hello World App/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello World App"),
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+        ),
+        body: Center(child: Text("Hello World")),
+      ),
+    );
+  }
+}
